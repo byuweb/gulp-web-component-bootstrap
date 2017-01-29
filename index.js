fefe23e86d0a42c1e5df9be861e1c3e1bb39273d
Joseph Moore <yo.yo.yossarian@gmail.com>
2017-01-28 21:43:26 -0700

    add script location detection

--- index.js
+++ index.js
@@ -4,6 +4,9 @@
 
 const through = require('through2');
 const Vinyl = require('vinyl');
+const path = require('path');
+const fs = require('fs');
+const ejs = require('ejs');
 
 module.exports = function bootstrap(options) {
     const opts = options || {};
@@ -15,41 +18,15 @@ module.exports = function bootstrap(options) {
         this.push(file);
         this.push(new Vinyl({
             path: opts.output || 'bootstrap.js',
-            contents: generateContent(file.path, opts.polyfills)
+            contents: generateContent(opts.main, opts.polyfills)
         }));
     });
 };
 
-function generateContent(target, polyfill) {
-    return `(function(opts) {
-    var byu = window.byu = window.byu || {};
-    var comps = byu.webCommunityComponents = byu.webCommunityComponents || {};
-    //You can set window.byu.webCommunityComponents.forcePolyfills to true to always use polyfills.
-    var forcePolyfills = comps.forcePolyfills;
-    //This is here because if we have multiple component bundles on the page and one of them has already loaded the
-    //  polyfills, we would erroneously detect that we don't need to load them and load the native ES6 code instead 
-    //  (which could cause problems).  So, we set 'needsPolyfills' to tell ourselves to ignore the feature detection.
-    var needsPolyfills;
-    if (!'needsPolyfills' in comps) {
-        var shadow = !!HTMLElement.prototype.attachShadow;
-        var customElements = 'customElements' in window;
-        
-        needsPolyfills = comps.needsPolyfills = !shadow || !customElements;
-    }   
-    if (needsPolyfills || forcePolyfills) {
-        load(opts.polyfills);
-    } else {
-        load(opts.main);
-    }
-    
-    function load(script) {
-        var main = document.createElement('script');
-        main.src = script;
-        main.async = true;
-        document.head.appendChild(main);
-    }
-})({
-    main: '${target}',
-    polyfills: '${polyfill}'
-});`;
+function generateContent(noPolyfills, polyfills) {
+    var template = fs.readFileSync(path.join(__dirname, 'bootstrap.template.js'), {encoding: 'utf8'});
+    return Buffer.from(ejs.render(template, {files: {
+        noPolyfills: noPolyfills,
+        polyfills: polyfills
+    }}));
 }
